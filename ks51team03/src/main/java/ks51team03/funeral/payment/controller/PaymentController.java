f4d51c7e688e6bf407660e82100b323327223026
DESKTOP-048HU3O\ECS <wodlr1310@naver.com>
2024-06-26 16:03:54 +0900

    작성자 : 송재익 작성일 : 06/26 작성내용 : 장례 페이지 수정

--- ks51team03/src/main/java/ks51team03/funeral/payment/controller/PaymentController.java
+++ ks51team03/src/main/java/ks51team03/funeral/payment/controller/PaymentController.java
@@ -1,5 +1,14 @@
 package ks51team03.funeral.payment.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PaymentController {
 
+    @GetMapping("funeral/funeral_confirm_payment")
+    public String reserveDetail() {
+
+        return "funeral/funeral_confirm_payment";
+    }
 }
