a1e7bbe523d6058de0015d54cc26c0b03ac2f523
Chande1 <chandelier125@gmail.com>
2024-06-27 10:16:49 +0900

    작성자:한송이 작성일자:2024-06-27 작성시간:10:16 작성내용:자유게시판 페이지 끝

--- ks51team03/src/main/java/ks51team03/board/controller/BoardController.java
+++ ks51team03/src/main/java/ks51team03/board/controller/BoardController.java
@@ -6,12 +6,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("boardCommonController")
 public class BoardController {
-	@GetMapping("/board/board_write_nomal")
-	public String userMainPage(Model model)
+	@GetMapping("/board/board_write_normal")
+	public String boardWriteNormalPage(Model model)
 	{
 		model.addAttribute("title","PAL");
 		
-		return "/board/board_write_nomal";
+		return "/board/board_write_normal";
 	}
 
+	
+	@GetMapping("/board/board_list_normal")
+	public String boardListNormalPage(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "/board/board_list_normal";
+	}
+	
+	@GetMapping("/board/board_view_normal")
+	public String boardViewNormalPage(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "/board/board_view_normal";
+	}
+	
+	@GetMapping("/board/board_edit_normal")
+	public String boardEditNormalPage(Model model)
+	{
+		model.addAttribute("title","PAL");
+		
+		return "/board/board_edit_normal";
+	}
 }
