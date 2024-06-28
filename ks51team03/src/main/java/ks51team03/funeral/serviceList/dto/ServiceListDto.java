083f50855615873eb5095abfe54a96d992661fcc
DESKTOP-048HU3O\ECS <wodlr1310@naver.com>
2024-06-28 11:06:13 +0900

    작성자 : 송재익 작성일 : 06/28 작성내용 : 장례 전체 서비스 db 보여주기

--- ks51team03/src/main/java/ks51team03/funeral/serviceList/dto/ServiceListDto.java
+++ ks51team03/src/main/java/ks51team03/funeral/serviceList/dto/ServiceListDto.java
@@ -1,5 +1,16 @@
 package ks51team03.funeral.serviceList.dto;
 
+import lombok.Data;
+
+@Data
 public class ServiceListDto {
 
+    private String funeralserviceCode;
+    private String funeralserviceCcode; // 업체코드
+    private String funeralserviceTitle; // 장례 서비스 이름
+    private int funeralservicePrice; // 서비스가격
+    private float funeralserviceWeight; // 허용무게(kg)
+    private String funeralserviceId; // 등록자 아이디
+    private String funeralserviceRegDate; // 등록일
+    private String funeralserviceState; //상태
 }
