62c6f6f521c4d4b2dc002bdb6a7be9ddd5c203c2
DESKTOP-048HU3O\ECS <wodlr1310@naver.com>
2024-07-09 14:46:46 +0900

    작성자 : 송재익 작성일 : 07/09 작성내용 : 결제 전 페이지 완

--- ks51team03/src/main/java/ks51team03/funeral/reserve/mapper/ReserveMapper.java
+++ ks51team03/src/main/java/ks51team03/funeral/reserve/mapper/ReserveMapper.java
@@ -2,7 +2,10 @@ package ks51team03.funeral.reserve.mapper;
 
 import ks51team03.funeral.reserve.dto.ReserveDto;
 import ks51team03.funeral.reserve.dto.ReserveInfoDto;
+import ks51team03.funeral.reserve.dto.ReserveServiceInfoDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ReserveMapper {
@@ -13,6 +16,9 @@ public interface ReserveMapper {
     String getLastFuneralServiceCode();
 
     // 장례 예약자 인적사항 확인하기
-    void funeralReserveInfo(ReserveInfoDto reserveInfoDto);
+    List<ReserveInfoDto> funeralReserveInfo(ReserveInfoDto reserveInfoDto);
+
+    // 장례 예약 내역 확인하기
+    List<ReserveServiceInfoDto> funeralReserveServiceInfo(ReserveServiceInfoDto reserveServiceInfoDto);
 
 }
